884621d0f09f639664d5f19020ce264151f8d43a
JungleBlame <149385056+JungleBlame@users.noreply.github.com>
2023-12-20 10:01:10 +0000

    File search

--- Williams_Ryan_RoutePlot.py
+++ Williams_Ryan_RoutePlot.py
@@ -1,1 +1,22 @@
 
+def drone_func():
+
+    route = input("Please enter a route file name or STOP to end: ")
+
+    if route.upper() == "STOP":
+        print("Ok, stopping")
+        return 
+    
+    else:
+        try:
+            open(route, "r")
+            coordinates=route.read()
+        except:
+            print ("File not found")
+            drone_func()
+    
+
+
+
+drone_func()
+
